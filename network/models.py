e456838940c03130f038b433b54684f1bebe3b6c
Davidvinayak <S210118@rguktsklm.ac.in>
2026-01-05 21:36:47 +0530

    added additional model to the project

--- network/models.py
+++ network/models.py
@@ -4,3 +4,25 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.timestamp}"
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers"
+    )
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
